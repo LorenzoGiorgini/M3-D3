f3ad2bd6d18a0489b01b6862b33a357e797b2096
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-09-15 16:57:01 +0200

    latest commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function putImage() {
             col.className = "col-4"
 
             col.innerHTML = `
-            <div class="card mb-4 shadow-sm">
+            <div class="card mb-4 shadow-sm" id="card">
                 <img src="${obj.src.medium}" class="img-resized">
                 <div class="card-body">
                   <div
@@ -34,12 +34,13 @@ function putImage() {
                       </button>
                       <button
                         type="button"
-                        class="btn btn-sm btn-outline-secondary"
+                        class="btn btn-sm hide-me btn-outline-secondary"
+                        onclick="hide()"
                       >
-                        Edit
+                        Hide
                       </button>
                     </div>
-                    <small class="text-muted">9 mins</small>
+                    <small class="text-muted">${obj.id}</small>
                   </div>
                 </div>
               </div>
@@ -52,13 +53,24 @@ function putImage() {
 
 
 const fetchApi = (type) => {
-    return fetch("https://api.pexels.com/v1/search?query=" + type, {
+    return fetch("https://api.pexels.com/v1/search?query=" + type.toLowerCase(), {
         "method": "GET",
         "headers": {
-            "Authorization": "563492ad6f91700001000001fa97653d5ab04b4b91c7a5e6d871f79c"
+            "Authorization": "563492ad6f91700001000001fe7f1bfa604c4313af35b3f3b8ea9b23"
         }
     })
 }
+
+
+const hide = () => {
+    let row = document.querySelector('#album-row');
+    let card = document.querySelector('#card');
+    row.removeChild(card)
+    console.log(2);
+};
+
+
+
 
 
 
